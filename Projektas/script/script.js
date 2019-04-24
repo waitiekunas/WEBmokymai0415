e12cdec95e176fc2c054852a6b88b52e87cf05be
Andrius Vaitiekunas <waitiekunas@gmail.com>
2019-04-24 18:29:40 +0300

    Added login elements and started the logic implementation

--- Projektas/script/script.js
+++ Projektas/script/script.js
@@ -1,15 +1,14 @@
-
 var fakerecipes = [
     {
         "id": 1,
         "Name": "First recipe",
-        "Ingredients": ["salt", "pepper", "sugar"],
+        "Ingredients": ["salt", "pepper", "sugar", "meat"],
         "Flow": "Some random text"
     },
     {
         "id": 2,
         "Name": "Second recipe",
-        "Ingredients": ["salt", "pepper", "sugar"],
+        "Ingredients": ["salt", "pepper", "sugar", "fish"],
         "Flow": "Some random text"
     },
     {
@@ -19,6 +18,43 @@ var fakerecipes = [
         "Flow": "Some random text"
     }
 ];
+
+var fakeUser = [
+    {
+        "id": 1,
+        "Login": "login1",
+        "Password": "Password1"
+    },
+    {
+        "id": 2,
+        "Login": "login2",
+        "Password": "Password2"
+    },
+    {
+        "id": 3,
+        "Login": "login3",
+        "Password": "Password3"
+    }
+
+];
+
+
+
+function signIn() {
+    let login = document.getElementById('login-name-input').value;
+    let password = document.getElementById('password-input').value;
+    checkExisting(login, password, fakeUser);
+};
+
+function checkExisting(login, password, data) {
+    data.forEach(user => {
+        if (user.Login === login && user.Password === password) {
+            console.log("match found");
+        } else {
+            console.log("no match found");
+        }
+    })
+}
 
 function listEntries(entries) {
     var counter = 1;
@@ -30,7 +66,7 @@ function listEntries(entries) {
         parentNode.classList.add('main-recipe');
 
         let pictureNode = document.createElement('IMG');
-        pictureNode.setAttribute('src', 'images/'+counter+'.jpg');
+        pictureNode.setAttribute('src', 'images/' + counter + '.jpg');
         pictureNode.classList.add('img-recipe')
 
         let node = document.createElement("P");
